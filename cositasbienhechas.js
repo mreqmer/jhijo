bebf95c77439f969ca061053a74996b8d37c45c7
mreqmer <145675425+mreqmer@users.noreply.github.com>
2024-04-06 12:53:48 +0200

    06/04

---
+++ cositasbienhechas.js
@@ -0,0 +1,50 @@
+function cadena(persona) {
+
+    return JSON.stringify(persona);
+
+};
+
+function newUser() {
+
+    var nombre = document.getElementById("nombre").value;
+    var apellidos = document.getElementById("apellidos").value;
+    var edad = document.getElementById("edad").value;
+    var ciudad = document.getElementById("ciudad").value;
+
+    let usuario = {
+        nombrePersona: nombre,
+        apellidosPersona: apellidos,
+        edadPersona: edad,
+        ciudadPersona: ciudad
+    }
+
+    newRow(usuario);
+
+
+};
+
+function newRow(persona) {
+
+    $("#tablaUsuarios").append("<tr>" +
+        "<td>" + persona.nombrePersona + "</td>" +
+        "<td>" + persona.apellidosPersona + "</td>" +
+        "<td>" + persona.edadPersona + "</td>" +
+        "<td>" + persona.ciudadPersona + "</td>" +
+        "</tr>")
+
+
+};
+
+
+
+/*function newRow(usuarioObj){ 
+
+    return "<tr>" + 
+                    "<td>" + usuarioObj.nombreU + "</td>" +
+                    "<td>" + usuarioObj.apellidosU + "</td>" +
+                    "<td>" + usuarioObj.edadU + "</td>" + 
+                    "<td>" + usuarioObj.ciudadU + "</td>" +
+            "</tr>"; 
+
+};
+*/
